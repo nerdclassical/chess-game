332df517eb6c6a00cd6c506cc1492337364ed37a
Marcos Antonio Ferreira Lima <127689054+nerdclassical@users.noreply.github.com>
2025-12-07 17:05:10 -0300

    concluindo trabalho grande de java

---
+++ Boardgame/Piece.java
@@ -0,0 +1,35 @@
+package Boardgame;
+
+
+public abstract class Piece {
+
+    protected Boardgame.Position position;
+    private Boardgame.Board board;
+
+    public Piece(Boardgame.Board board) {
+        this.board = board;
+        position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+
+    public abstract boolean[][] possibleMoves();
+
+    public boolean possibleMove(Boardgame.Position position) {
+        return possibleMoves()[position.getRow()][position.getColumn()];
+    }
+
+    public boolean isThereAnyPossibleMove() {
+        boolean[][] mat = possibleMoves();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[i][j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
